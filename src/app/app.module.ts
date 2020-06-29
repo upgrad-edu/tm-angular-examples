1e44c416bafb66318d6fc8e52d7a9731f520c3cb
Saurav Kumar <thekumar101@gmail.com>
2020-06-29 20:29:47 +0530

    session1 | Clean up

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,9 @@ import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { CoreModule } from './core/core.module';
-import { HomeModule } from './home/home.module';
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, CoreModule, HomeModule],
+  imports: [BrowserModule, AppRoutingModule],
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
   providers: [],
   bootstrap: [AppComponent],
--- src/app/customer.ts
+++
@@ -1,9 +0,0 @@
-export interface Customer {
-    customerId: string;
-    firstName: string;
-    lastName: string;
-    username: string;
-    password: string;
-    dateOfBirth: Date;
-    userTypeId: string;
-}
