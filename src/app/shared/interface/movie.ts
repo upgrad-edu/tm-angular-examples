c4cfa2a0e867f98124d1259f2162ce97ba337cd0
Saurav Kumar <thekumar101@gmail.com>
2020-07-03 13:52:15 +0530

    session5 | Init

--- src/app/shared/interface/movie.ts
+++ src/app/shared/interface/movie.ts
@@ -8,5 +8,33 @@ export interface Movie {
   languageId: string;
   statusId: string;
   theatres: any[];
-  id: number;
+  id?: number;
+}
+
+export class Movie {
+  coverURL: string;
+  description: string;
+  duration: string;
+  name: string;
+  releaseDate: string;
+  trailerURL: string;
+  languageId: string;
+  statusId: string;
+  theatres: any[];
+  id?: number;
+
+  constructor(coverURL: string, description: string, 
+    duration: string, name: string, releaseDate: string,
+    trailerURL: string, languageId: string, statusId: string, theatres: any[]
+  ) {
+    this.coverURL = coverURL;
+    this.description = description;
+    this.duration = duration;
+    this.name = name;
+    this.releaseDate = releaseDate;
+    this.trailerURL = trailerURL;
+    this.languageId = languageId;
+    this.statusId = statusId;
+    this.theatres = theatres;
+  }
 }
